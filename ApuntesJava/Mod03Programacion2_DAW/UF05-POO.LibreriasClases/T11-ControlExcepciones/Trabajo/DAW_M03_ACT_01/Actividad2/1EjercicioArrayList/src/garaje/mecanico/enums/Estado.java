388a7671c35ae7146ab2ef2a3fb073a892a2fb84
g4rc1ss <garciss@protonmail.com>
2021-08-18 16:38:53 +0200

    Apuntes de Java del grado superior agregados

--- ApuntesJava/Mod03Programacion2_DAW/UF05-POO.LibreriasClases/T11-ControlExcepciones/Trabajo/DAW_M03_ACT_01/Actividad2/1EjercicioArrayList/src/garaje/mecanico/enums/Estado.java
+++
@@ -1,6 +0,0 @@
-package garaje.mecanico.enums;
-
-public enum Estado {
-    recibida_comenzada,
-    finalizada
-}
--- ApuntesJava/Mod03Programacion2_DAW/UF05-POO.LibreriasClases/T11-ControlExcepciones/Trabajo/DAW_M03_ACT_01/Actividad2/1EjercicioArrayList/src/garaje/mecanico/enums/TipoTrabajo.java
+++
@@ -1,7 +0,0 @@
-package garaje.mecanico.enums;
-
-public enum TipoTrabajo {
-    reparacionMecanica,
-    reparacionChapaPintura,
-    revision
-}
