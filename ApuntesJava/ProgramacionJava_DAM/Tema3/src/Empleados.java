5743047f9a267de1bccdabe9b44e71734dbfc718
g4rc1ss <garciss@protonmail.com>
2021-08-18 13:25:20 +0200

    update

--- ApuntesJava/ProgramacionJava_DAM/Tema3/src/Empleados.java
+++
@@ -1,6 +0,0 @@
-/*crear una clase llamada empleado con los metodos: inicializar (se teclea nombre y sueldo del empleado), 
- pagarImpuestos (que visualice "debe pagar impuestos" si su sueldo es mayor a 20.000 y 
- "no debe pagar impuestos" si su sueldo es menor o igual a 20.000). */
-public class Empleados {
-
-}
