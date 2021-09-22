264b3cb1ba71ed1e1e396fb657fefdd31b766692
g4rc1ss <garciss@protonmail.com>
2021-09-22 13:16:01 +0200

    actualizar archivos

--- ApuntesNodeJS/ApuntesNodeJS/Web/API_ExpressJS/src/index.ts
+++ ApuntesNodeJS/ApuntesNodeJS/Web/API_ExpressJS/src/index.ts
@@ -1,5 +1,19 @@
-import { PruebaFront } from ".\\_01_Frontend\\pruebaFront";
+import express from "express";
+import { HolaController } from ".\\_01_Controllers\\holaController";
 
-let llamadaFront: PruebaFront = new PruebaFront("");
+const app: any = express();
+new HolaController().set(app);
+const PORT = 8525;
 
-llamadaFront.saludar();
+
+
+app.get('/', (req, res) => {
+    res.send('Index de la pagina');
+});
+
+app.listen(PORT, err => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${PORT}`);
+});
