655eacd7d497872d08c5df498aa98148a2245404
g4rc1ss <garciss@protonmail.com>
2021-09-23 23:53:00 +0200

    Crear proyecto de comprobacion de simbolos en strings

--- ApuntesNet/ApuntesNet/BackEnd/Algoritmos/AnalisisAperturaCierreSimbolos/Program.cs
+++ ApuntesNet/ApuntesNet/BackEnd/Algoritmos/AnalisisAperturaCierreSimbolos/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace AnalisisAperturaCierreSimbolos
-{
-    class Program
-    {
-        const string PRUEBA1 = "[[[{{()}}]]]";
-        const string PRUEBA2 = "[[[{{(}]]]";
+namespace AnalisisAperturaCierreSimbolos {
+    internal class Program {
+        private const string PRUEBA1 = "[[[{{()}}]]]";
+        private const string PRUEBA2 = "[[[{{(}]]]";
+        private static Dictionary<char, char> simbolosAperturaCierre = new() {
+            { '(', ')' },
+            { '{', '}' },
+            { '[', ']' }
+        };
 
-        static void Main(string[] args)
-        {
-            
+        private static void Main(string[] args) {
+            var resultado = default(bool);
+            Console.WriteLine("------------------ PRUEBA DE SIMBOLOS CORRECTOS ------------------");
+            resultado = ComprobacionStringSimbolos(PRUEBA1);
+            Console.WriteLine($"Resultado = {resultado}");
+
+            Console.WriteLine("------------------ PRUEBA DE SIMBOLOS INCORRECTOS ------------------");
+            resultado = ComprobacionStringSimbolos(PRUEBA2);
+            Console.WriteLine($"Resultado = {resultado}");
+        }
+
+        private static bool ComprobacionStringSimbolos(string frase) {
+            var stack = new Stack<char>();
+            foreach (var item in frase) {
+                if (simbolosAperturaCierre.ContainsKey(item)) {
+                    stack.Push(item);
+                } else if (simbolosAperturaCierre.ContainsValue(item) && simbolosAperturaCierre[stack.Peek()] == item) {
+                    stack.Pop();
+                } else {
+                    break;
+                }
+            }
+            return stack.Count == 0;
         }
     }
 }
