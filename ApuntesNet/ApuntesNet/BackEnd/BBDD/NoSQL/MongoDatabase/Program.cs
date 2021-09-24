175f9eebe14fc24b10d108effd414e048940b564
g4rc1ss <garciss@protonmail.com>
2021-09-24 12:42:46 +0200

    Cambiar estructura bbdd

---
+++ ApuntesNet/ApuntesNet/BackEnd/BBDD/NoSQL/MongoDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
