f36396b62c8b8115f023428d8a058d7917a7718b
g4rc1ss <garciss@protonmail.com>
2021-09-28 14:57:34 +0200

    Crear memoizacion
    Crear el proyecto para memoizar una operacion recursiva como es fibonacci

--- ApuntesNet/ApuntesNet/BackEnd/Algoritmos/Memorization/Program.cs
+++ ApuntesNet/ApuntesNet/BackEnd/Algoritmos/Memorization/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Memorization.Clase;
 
-namespace Memorization
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Memorization {
+    internal class Program {
+        private static void Main(string[] args) {
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            Console.WriteLine(CalcularFibonacci(50));
+            stopWatch.Stop();
+            var tiempoFuncionNormal = stopWatch.Elapsed.TotalMilliseconds;
+
+            stopWatch.Reset();
+
+            stopWatch.Start();
+            Console.WriteLine(CalcularFibConMemoizacion(50));
+            stopWatch.Stop();
+            var tiempoFuncionMemoizacion = stopWatch.Elapsed.TotalMilliseconds;
+
+
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine($"EL TIEMPO DE {nameof(CalcularFibonacci)} es: {tiempoFuncionNormal}\n" +
+                $"EL TIEMPO DE {nameof(CalcularFibConMemoizacion)} es: {tiempoFuncionMemoizacion}");
+        }
+
+        private static long CalcularFibonacci(long numero) {
+            Console.WriteLine($"Calculando el fib de {numero}");
+            return numero > 1 ? CalcularFibonacci(numero - 1) + CalcularFibonacci(numero - 2) : numero;
+        }
+
+        private static long CalcularFibConMemoizacion(long numero) {
+            Console.WriteLine($"Calculando el fib de {numero}");
+            return numero > 1
+                ? Memoizacion<long, long>.AddMemoizacion(CalcularFibConMemoizacion, numero - 1) + Memoizacion<long, long>.AddMemoizacion(CalcularFibConMemoizacion, numero - 2)
+                : numero;
         }
     }
 }
