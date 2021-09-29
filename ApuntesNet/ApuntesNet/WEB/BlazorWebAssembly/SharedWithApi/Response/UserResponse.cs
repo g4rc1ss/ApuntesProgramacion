f958a2ec9a7c8ffa8f807f759e71507742c5fdcf
g4rc1ss <garciss@protonmail.com>
2021-09-29 22:40:48 +0200

    Post en blazor
    Crea una prueba de concepto para enviar un objeto desde blazor a un webapi

--- ApuntesNet/ApuntesNet/WEB/BlazorWebAssembly/SharedWithApi/Response/UserResponse.cs
+++ ApuntesNet/ApuntesNet/WEB/BlazorWebAssembly/SharedWithApi/Response/UserResponse.cs
@@ -1,4 +1,4 @@
-﻿namespace BlazorWebAssembly {
+﻿namespace BlazorWebAssembly.SharedWithApi.Response {
     public class UserResponse {
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
