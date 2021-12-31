35d504b6d5d44b020812dc3c779a8205a6217ded
g4rc1ss <asiergarciab@outlook.com>
2021-12-31 15:44:10 +0100

    Agregar query parameterizadas
    Update SelectData.cs

--- ApuntesNet/ApuntesNet/BackEnd/BBDD/ORM/SqliteDapper/Dam/DeleteData.cs
+++ ApuntesNet/ApuntesNet/BackEnd/BBDD/ORM/SqliteDapper/Dam/DeleteData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Dapper;
 using SqliteDapper.Database;
 using SqliteDapper.Database.Sqlite;
@@ -7,15 +8,17 @@ namespace SqliteDapper.Dam
 {
     internal static class DeleteData
     {
-        internal static void DeleteDataQuery()
+        internal static async Task DeleteDataQueryAsync()
         {
-            using (var connection = DapperExecute.GetConnection())
+            using var connection = DapperExecute.GetConnection();
+            
+            var deleteUsuario = @$"DELETE FROM {nameof(Usuario)} WHERE Id = @idUsuario";
+            var nChangesUsuario = await connection.ExecuteAsync(deleteUsuario.ToString(), new
             {
-                var deleteUsuario = @$"DELETE FROM {nameof(Usuario)} WHERE Id = 'IdUsuario2'";
-                var nChangesUsuario = connection.Execute(deleteUsuario.ToString());
+                idUsuario = "IdUsuario2"
+            });
 
-                Console.WriteLine($"Borrado {nChangesUsuario} registros");
-            }
+            Console.WriteLine($"Borrado {nChangesUsuario} registros");
         }
     }
 }
