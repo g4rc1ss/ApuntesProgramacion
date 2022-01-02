189f04c333e5fbc135d3be1aec76de5c08a64de4
g4rc1ss <asiergarciab@outlook.com>
2022-01-02 17:37:57 +0100

    Crear el proyecto MemoryStream

---
+++ Lenguajes/.Net/ApuntesNet/BackEnd/EscrituraLectura/EnMemoria/Escritura/Escribir.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace EnMemoria.Escritura
+{
+    internal class Escribir
+    {
+        internal async Task<MemoryStream> Write()
+        {
+            byte[] firstString = new UnicodeEncoding().GetBytes("Texto a convertir en bytes");
+            byte[] secondString = new UnicodeEncoding().GetBytes("Texto a agregar");
+
+            var memoryStream = new MemoryStream();
+            
+            memoryStream.Write(firstString, 0, firstString.Length);
+
+            foreach (var item in secondString)
+            {
+                memoryStream.WriteByte(item);
+            }
+
+            return memoryStream;
+        }
+    }
+}
---
+++ Lenguajes/.Net/ApuntesNet/BackEnd/EscrituraLectura/EnMemoria/Program.cs
@@ -0,0 +1,8 @@
+﻿using EnMemoria.Copia;
+using EnMemoria.Escritura;
+using EnMemoria.Lectura;
+
+
+var streamEscrito = await new Escribir().Write();
+await new Leer().Read(streamEscrito);
+await new Copiar().Copy(streamEscrito);
