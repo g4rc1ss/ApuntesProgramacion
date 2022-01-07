d43071d229f8a53b112af3cd7388be5ec3132285
g4rc1ss <asiergarciab@outlook.com>
2022-01-07 22:22:30 +0100

    Crear Arquitectura Hexagonal

---
+++ Lenguajes/.Net/ApuntesNet/Arquitectura/Hexagonal/HexagonalArchitecture.Application/Ports/UserPort/IClaseSimuloBaseDeDatos.cs
@@ -0,0 +1,9 @@
+﻿using HexagonalArchitecture.Application.Dto;
+
+namespace HexagonalArchitecture.Application.Ports.UserPort
+{
+    public interface IClaseSimuloBaseDeDatos
+    {
+        Task<List<UserDto>> UserList();
+    }
+}
---
+++ Lenguajes/.Net/ApuntesNet/Arquitectura/Hexagonal/HexagonalArchitecture.DB/DependencyDbFactory.cs
@@ -0,0 +1,12 @@
+﻿using HexagonalArchitecture.DB.UserDb;
+
+namespace HexagonalArchitecture.DB
+{
+    public class DependencyDbFactory
+    {
+        public static ClaseSimuloBaseDeDatos GetSimulacionBaseDatos()
+        {
+            return new();
+        }
+    }
+}
