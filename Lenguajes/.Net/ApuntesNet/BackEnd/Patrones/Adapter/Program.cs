b78dcb3bd0804224d0bb29ef8630ef012f1514ab
g4rc1ss <asiergarciab@outlook.com>
2022-01-09 00:45:31 +0100

    Patron fachada, proxy y Adapter

--- Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Adapter/Program.cs
+++ Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Adapter/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Adapter;
+
+var adaptadar = new Adaptar();
+ITarget target = new PatronAdapter(adaptadar);
+
+Console.WriteLine("El objeto de la clase Adaptar que nos viene, no es compatible con el cliente");
+Console.WriteLine("Pero gracias a la clase PatronAdapter, conseguimos que lo sea");
+
+Console.WriteLine(target.GetRequest());
+
+Console.ReadKey();
