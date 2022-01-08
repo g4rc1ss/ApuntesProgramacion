aa570e5618016142156f2da55e5f5dfb43aa122d
g4rc1ss <asiergarciab@outlook.com>
2022-01-09 00:08:37 +0100

    Patron Flyweight

---
+++ Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Flyweight/ObjetoGuardar.cs
@@ -0,0 +1,14 @@
+﻿namespace Flyweight
+{
+    internal class ObjetoGuardar
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Descripcion { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nombre} - {Apellidos} - {Descripcion}";
+        }
+    }
+}
