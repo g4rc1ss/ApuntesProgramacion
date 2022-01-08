a41333e757614ed555c1ed7bfe43ea3f8432dc02
g4rc1ss <asiergarciab@outlook.com>
2022-01-08 22:41:18 +0100

    Patron Singleton

---
+++ Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Singleton/PatronSingleton.cs
@@ -0,0 +1,24 @@
+﻿namespace Singleton
+{
+    public class PatronSingleton
+    {
+        private static PatronSingleton _singleton;
+
+        public string Nombre { get; set; }
+
+        private PatronSingleton()
+        {
+
+        }
+
+        public static PatronSingleton GetInstance()
+        {
+            if (_singleton == null)
+            {
+                _singleton = new PatronSingleton();
+            }
+
+            return _singleton;
+        }
+    }
+}
