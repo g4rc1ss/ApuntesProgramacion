cb1230dbce54b2374670b5903e1530d3294e636f
g4rc1ss <asiergarciab@outlook.com>
2022-01-08 23:03:34 +0100

    Patron Prototype

---
+++ Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Prototype/ClasePrototype.cs
@@ -0,0 +1,32 @@
+﻿namespace Prototype
+{
+    public class ClasePrototype : ICloneable
+    {
+        private readonly string _password;
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Age { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public ClasePrototype(string password)
+        {
+            _password = password;
+        }
+
+        public object Clone()
+        {
+            return new ClasePrototype(_password)
+            {
+                Name = Name,
+                Description = Description,
+                Age = Age,
+                FechaNacimiento = FechaNacimiento,
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Description} - {Age} - {_password}";
+        }
+    }
+}
--- Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Prototype/Program.cs
+++ Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Prototype/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Prototype;
+
+var prototype = new ClasePrototype("contraseña 1")
+{
+    Name = "Nombre",
+    Description = "Descripcion",
+    Age = 20,
+    FechaNacimiento = DateTime.MinValue,
+};
+Console.WriteLine(prototype.ToString());
+
+var copia = (ClasePrototype)prototype.Clone();
+Console.WriteLine(copia.ToString());
+
+Console.WriteLine("------------------------ cambios ------------------------");
+
+prototype.Name = "Nombre modificado";
+prototype.Description = "Descripcion modificada";
+prototype.Age = 12;
+prototype.FechaNacimiento = DateTime.Now;
+Console.WriteLine(prototype.ToString());
+
+copia.Name = "Nombre copia modificada";
+copia.Description = "Descripcion copia modificada";
+copia.Age = 56;
+copia.FechaNacimiento = DateTime.Now;
+Console.WriteLine(copia.ToString());
+
+Console.ReadKey();
