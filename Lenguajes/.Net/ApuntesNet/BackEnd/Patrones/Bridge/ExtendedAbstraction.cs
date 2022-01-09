9f81ade53fdb6199dd09495ca6e5daacd0da60fa
g4rc1ss <asiergarciab@outlook.com>
2022-01-09 01:07:10 +0100

    Patron Bridge

---
+++ Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Bridge/ExtendedAbstraction.cs
@@ -0,0 +1,17 @@
+﻿namespace Bridge
+{
+    // You can extend the Abstraction without changing the Implementation
+    // classes.
+    internal class ExtendedAbstraction : Abstraction
+    {
+        public ExtendedAbstraction(IImplementation implementation) : base(implementation)
+        {
+        }
+
+        public override string Operation()
+        {
+            return "ExtendedAbstraction: Extended operation with:\n" +
+                _implementation.OperationImplementation();
+        }
+    }
+}
--- Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Bridge/Program.cs
+++ Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Bridge/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bridge;
+
+var client = new Client();
+
+Abstraction abstraction;
+// The client code should be able to work with any pre-configured
+// abstraction-implementation combination.
+abstraction = new Abstraction(new ConcreteImplementationA());
+client.ClientCode(abstraction);
+
+Console.WriteLine();
+
+abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+client.ClientCode(abstraction);
+
+Console.ReadKey();
