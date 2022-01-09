4fcd692f04dee76061a89b7874cab30bcbb4c6a3
g4rc1ss <asiergarciab@outlook.com>
2022-01-09 21:51:54 +0100

    Crear patron Command y ChainOfResponsability

---
+++ Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/ChainOfResponsability/DogHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace ChainOfResponsability
+{
+    internal class DogHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "MeatBall")
+            {
+                return $"Dog: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+}
