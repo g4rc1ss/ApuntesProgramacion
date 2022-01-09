69666b4d2346508031ceee9c8541a8b5f7b2aa33
g4rc1ss <asiergarciab@outlook.com>
2022-01-09 19:01:22 +0100

    Patron Iterator

--- Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Iterator/Program.cs
+++ Lenguajes/.Net/ApuntesNet/BackEnd/Patrones/Iterator/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Iterator;
+
+var coleccionPersonalizado = new EnumerablePersonalizado<string>(2);
+coleccionPersonalizado.enumerable[0] = "Hola";
+coleccionPersonalizado.enumerable[1] = "Adios";
+
+foreach (var item in coleccionPersonalizado)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Pulsa una tecla para finalizar");
+Console.ReadKey();
