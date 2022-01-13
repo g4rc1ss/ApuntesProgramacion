706cdb4e0c9a4bdf13ef7723dc22bf0c74d22cdd
g4rc1ss <asiergarciab@outlook.com>
2022-01-13 01:15:32 +0100

    Actualizar llamada a API

--- Lenguajes/.Net/ApuntesNet/BackEnd/ConexionesInternet/UseApiWeb/Program.cs
+++ Lenguajes/.Net/ApuntesNet/BackEnd/ConexionesInternet/UseApiWeb/Program.cs
@@ -1,11 +1,21 @@
-﻿namespace UseApiWeb
+﻿using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+var client = new HttpClient();
+var yearList = Enumerable.Range(2015, 7);
+var daysList = Enumerable.Range(1, 25);
+var current = Directory.GetCurrentDirectory();
+
+
+await Parallel.ForEachAsync(yearList, async (year, token) =>
 {
-    internal class Program
+    Directory.CreateDirectory(current + $"/{year}");
+    await Parallel.ForEachAsync(daysList, async (day, token) =>
     {
-        private static void Main()
-        {
-            // -------- Conexion para descargar archivos por REQUEST -------- \\
-            JSON.DescargarJSON.DescargarRequestJSON();
-        }
-    }
-}
+        var response = await client.GetStringAsync($"https://adventofcode.com/{year}/day/{day}", token);
+        Directory.CreateDirectory(current + $"/{year}/{day}");
+        await File.WriteAllTextAsync(current + $"/{year}/{day}/enunciado.md", response);
+    });
+});
