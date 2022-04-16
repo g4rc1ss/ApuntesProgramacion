f2ecbb0fdef164472cb1b6e1f2914fcaafcbeee9
g4rc1ss <asiergarciab@outlook.com>
2022-04-17 00:08:58 +0200

    Create ProfileAutoMapper.cs

---
+++ Lenguajes/.Net/ApuntesNet/Arquitectura/CleanArchitecture/CleanArchitecture.Ejemplo/AutoMapperProfiles/ProfileAutoMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArchitecture.ApplicationCore.Domain.Negocio.Filtros.UserDetail;
+using CleanArchitecture.Shared.Peticiones.Request.Users.UserDetail;
+
+namespace CleanArchitecture.Ejemplo.AutoMapperProfiles
+{
+    public class ProfileAutoMapper : Profile
+    {
+        public ProfileAutoMapper()
+        {
+            CreateMap<UserDetailRequest, FiltroUser>();
+
+        }
+    }
+}
