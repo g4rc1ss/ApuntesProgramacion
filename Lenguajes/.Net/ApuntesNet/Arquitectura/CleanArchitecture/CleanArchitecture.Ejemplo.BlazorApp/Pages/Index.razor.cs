a9d29244efcc6bb8d2ddfdc9c1f418224b91b7ab
g4rc1ss <asiergarciab@outlook.com>
2022-04-19 12:19:24 +0200

    Crear aplicacion Blazor que consulta API CleanArchitecture

--- Lenguajes/.Net/ApuntesNet/Arquitectura/CleanArchitecture/CleanArchitecture.Ejemplo.BlazorApp/Pages/Index.razor.cs
+++ Lenguajes/.Net/ApuntesNet/Arquitectura/CleanArchitecture/CleanArchitecture.Ejemplo.BlazorApp/Pages/Index.razor.cs
@@ -9,11 +9,32 @@ namespace CleanArchitecture.Ejemplo.BlazorApp.Pages
         [Inject]
         private HttpClient _httpClient { get; set; }
 
+        private string? ShowModal { get; set; }
+        private string? Display { get; set; } = "d-none";
+
         internal List<UserResponse>? UsersResponse { get; set; }
+        internal UserResponse? UserDetailResponse { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             UsersResponse = await _httpClient.GetFromJsonAsync<List<UserResponse>>("ListaUsuarios/ObtenerListadoUsuarios");
+        }
+
+        public async Task SetModalOpenAsync(int idUsuario)
+        {
+            UserDetailResponse = await _httpClient.GetFromJsonAsync<UserResponse>($"UsuariosDetalle/ObtenerDetalleUsuarioGet/{idUsuario}");
+            Display = "d-block";
+            await Task.Delay(10);
+            ShowModal = "show";
+            StateHasChanged();
+        }
+
+        public async Task SetModalCloseAsync()
+        {
+            ShowModal = "";
+            await Task.Delay(400);
+            Display = "d-none";
+            StateHasChanged();
         }
     }
 }
