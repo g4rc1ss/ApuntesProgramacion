5c4a169a06b848f864993b826703d5ef2cb3e7e5
g4rc1ss <asiergarciab@outlook.com>
2022-05-10 17:52:47 +0200

    Agregar AuthenticationCookie con HttpContext

---
+++ Lenguajes/.Net/ApuntesNet/Web/AuthenticationWithCookieRazor/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Claims;
+using AuthenticationWithCookieRazor.Peticiones.Request;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CleanArchitecture.Ejemplo.RazorPages.Pages.Account
+{
+    [AllowAnonymous]
+    public class LoginModel : PageModel
+    {
+        public LoginModel()
+        {
+
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public LoginRequest LoginRequest { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string ReturnUrl { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnGetLoginAsync()
+        {
+            return await OnPostLoginAsync();
+        }
+
+        public async Task<IActionResult> OnPostLoginAsync()
+        {
+            var respuestaLogin = new
+            {
+                Codigo = 1,
+                NombreUsuario = LoginRequest.UserName,
+                RoleUsuario = "Usuario",
+            };
+
+            var identity = new ClaimsIdentity("Cookies");
+            identity.AddClaim(new Claim(ClaimTypes.Role, respuestaLogin.RoleUsuario));
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, respuestaLogin.Codigo.ToString()));
+            identity.AddClaim(new Claim(ClaimTypes.Name, respuestaLogin.NombreUsuario));
+
+            var prop = new AuthenticationProperties { IsPersistent = false };
+            await HttpContext.SignInAsync("Cookies", new ClaimsPrincipal(identity), prop);
+
+            return LocalRedirect(ReturnUrl ?? "/");
+        }
+    }
+}
---
+++ Lenguajes/.Net/ApuntesNet/Web/AuthenticationWithCookieRazor/Peticiones/Request/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthenticationWithCookieRazor.Peticiones.Request
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
