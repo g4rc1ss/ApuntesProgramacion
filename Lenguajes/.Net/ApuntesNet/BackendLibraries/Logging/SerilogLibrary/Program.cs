7921d4500ea63624add14e01f762add5019278c7
g4rc1ss <asiergarciab@outlook.com>
2022-05-11 21:22:35 +0200

    Crear Serilog

--- Lenguajes/.Net/ApuntesNet/BackendLibraries/Logging/SerilogLibrary/Program.cs
+++ Lenguajes/.Net/ApuntesNet/BackendLibraries/Logging/SerilogLibrary/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AutomapperLibrary;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var serviceProvider = Helper.GetServiceProvider();
+
+var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+logger.LogDebug("Log para Depuracion");
+logger.LogInformation("Log para Informacion");
+logger.LogWarning("Log para trazas importantes");
+logger.LogError("Log para errores");
+logger.LogCritical("Log generalmente para excepciones");
+
+Console.Read();
