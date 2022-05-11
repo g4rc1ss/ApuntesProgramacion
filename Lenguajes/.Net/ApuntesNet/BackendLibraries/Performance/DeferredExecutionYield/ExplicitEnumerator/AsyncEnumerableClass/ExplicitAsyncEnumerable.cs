b19cb027edbc99be4b7df903a4a39c6cfbe1dac6
g4rc1ss <asiergarciab@outlook.com>
2022-05-11 15:29:04 +0200

    Crear ejempllo de ejecucion diferida

---
+++ Lenguajes/.Net/ApuntesNet/BackendLibraries/Performance/DeferredExecutionYield/ExplicitEnumerator/AsyncEnumerableClass/ExplicitAsyncEnumerable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredExecutionYield.ExplicitEnumerator.AsyncEnumerableClass
+{
+    internal class ExplicitAsyncEnumerable : IAsyncEnumerable<int>
+    {
+        public IAsyncEnumerator<int> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            return new ExplicitAsyncEnumerator();
+        }
+    }
+}
---
+++ Lenguajes/.Net/ApuntesNet/BackendLibraries/Performance/DeferredExecutionYield/ExplicitEnumerator/AsyncEnumerableClass/ExplicitAsyncEnumerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredExecutionYield.ExplicitEnumerator.AsyncEnumerableClass
+{
+    internal class ExplicitAsyncEnumerator : IAsyncEnumerator<int>
+    {
+        public int Current { get; private set; }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Task.Delay(1);
+            GC.SuppressFinalize(this);
+        }
+
+        public async ValueTask<bool> MoveNextAsync()
+        {
+            if (Current < 100)
+            {
+                Current++;
+                return await Task.FromResult(true);
+            }
+            return await Task.FromResult(false);
+        }
+    }
+}
