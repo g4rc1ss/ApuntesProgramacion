105723e17ecc1e05db3d51e8db124f458abbe837
Asier <asiergarciab@outlook.com>
2023-01-30 16:13:45 +0100

    update

---
+++ Lenguajes/Dotnet/ApuntesNet/Desktop/Gui/Calculadora/Componente.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace Calculadora;
+
+public class Componente : Button
+{
+
+    public Componente()
+    {
+        WidthRequest = 30;
+        HeightRequest = 30;
+    }
+
+    private string texto;
+
+    [Category("Opcion")]
+    public int Tipo { set; get; }
+
+    public string Accion(Editor cajaDeTexto)
+    {
+        if (Tipo == 0)
+        {
+            return null;
+        }
+        else if (Tipo == 1)
+        {//Numeros
+            texto += Text.ToString();
+            cajaDeTexto.Text += texto.ToString();
+            return texto;
+        }
+        else if (Tipo == 2)
+        {//Operacion
+            return Text.ToString();
+        }
+        else if (Tipo == 3)
+        {//Vaciar
+            cajaDeTexto.Text = "";
+            return Text.ToString();
+        }
+        return null;
+    }
+}
--- Lenguajes/Dotnet/ApuntesNet/Desktop/Gui/Calculadora/MainPage.xaml.cs
+++ Lenguajes/Dotnet/ApuntesNet/Desktop/Gui/Calculadora/MainPage.xaml.cs
@@ -2,24 +2,114 @@
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
+    private string operacion = "";
+    private readonly int[] numero = new int[2];
+    private bool insertar = true;
+    private bool operacionSeleccion = false;
 
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    void Listener(object sender, EventArgs e)
     {
-        count++;
+        Lanzar(sender);
+    }
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+    public async void Lanzar(object sender)
+    {
+        var compo = (Componente)sender;
+        var respuesta = compo.Accion(mostrar);
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        if (ComprobarNumero(respuesta))
+        {
+            if (insertar)
+            {
+                numero[0] = int.Parse(mostrar.Text);
+            }
+            else if (!insertar)
+            {
+                numero[1] = int.Parse(mostrar.Text);
+            }
+        }
+        else if (respuesta.Equals("+") || respuesta.Equals("-") || respuesta.Equals("*") || respuesta.Equals("/"))
+        {
+            if (mostrar.Text.Equals(""))
+            {
+                await DisplayAlert("Calculadora", "Calculadora", "Debes introducir un numero primero", "Cancel");
+                return;
+            }
+            if (operacionSeleccion)
+            {
+                await DisplayAlert("Calculadora", "Calculadora", "Ya has seleccionado una operacion", "Cancel");
+                return;
+            }
+            else
+            {
+                mostrar.Text = "";
+                operacion = respuesta;
+                insertar = false;
+                operacionSeleccion = true;
+            }
+
+        }
+        else if (respuesta.Equals("C"))
+        {
+            for (var x = 0; x < numero.Length; x++)
+            {
+                numero[x] = 0;
+            }
+
+            insertar = true;
+            operacionSeleccion = false;
+        }
+        else if (respuesta.Equals("="))
+        {
+            if (operacion == null)
+            {
+                await DisplayAlert("Calculadora", "Debes seleccionar una operacion primero", "Cancel");
+                return;
+            }
+            if (mostrar.Text.Equals(""))
+            {
+                await DisplayAlert("Calculadora", "Debes introducir otro numero", "Cancel");
+                return;
+            }
+            var resultado = Operar(operacion);
+            mostrar.Text = "" + resultado;
+            numero[0] = resultado;
+            insertar = true;
+            operacionSeleccion = false;
+        }
+    }
+
+    private int Operar(string operacion)
+    {
+        switch (operacion)
+        {
+            case "+":
+                return numero[0] + numero[1];
+            case "-":
+                return numero[0] - numero[1];
+            case "*":
+                return numero[0] * numero[1];
+            case "/":
+                return numero[0] / numero[1];
+        }
+        return 0;
+    }
+
+    private bool ComprobarNumero(string respuesta)
+    {
+        try
+        {
+            int.Parse(respuesta);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 }
-
-
