539c765cd11b82166c408958d54cd60656df418a
Asier <asiergarciab@outlook.com>
2023-09-23 16:57:21 +0200

    Crear servicios Worker

---
+++ Lenguajes/Dotnet/ApuntesNet/BackendLibraries/HostedService/Program.cs
@@ -0,0 +1,15 @@
+﻿using DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureServices(services =>
+{
+    services.AddSingleton<IServicioInyectado, ServicioInyectado>();
+    services.AddHostedService<HostedService.HostedService>();
+});
+
+var app = builder.Build();
+
+await app.StartAsync();
---
+++ Lenguajes/Dotnet/ApuntesNet/BackendLibraries/HostedService/ServicioInyectado.cs
@@ -0,0 +1,18 @@
+﻿namespace DependencyInjection;
+
+public interface IServicioInyectado
+{
+    Task<int> ExecuteAsync(CancellationToken cancellationToken = default);
+}
+
+public class ServicioInyectado : IServicioInyectado
+{
+    public int _counter = 0;
+
+    public Task<int> ExecuteAsync(CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine("Ejecutando el Servicio Inyectado");
+        _counter ++;
+        return Task.FromResult(_counter);
+    }
+}
