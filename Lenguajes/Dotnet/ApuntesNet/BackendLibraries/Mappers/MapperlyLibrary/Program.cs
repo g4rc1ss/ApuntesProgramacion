f94ef78fa5a5c1ae40f251fc8b99656691dd89e2
Asier <asiergarciab@outlook.com>
2023-12-08 21:44:33 +0100

    Update Mapperly Backend Lib

--- Lenguajes/Dotnet/ApuntesNet/BackendLibraries/Mappers/MapperlyLibrary/Program.cs
+++ Lenguajes/Dotnet/ApuntesNet/BackendLibraries/Mappers/MapperlyLibrary/Program.cs
@@ -5,6 +5,9 @@ var _mapperly = new MapperlyProfile();
 
 var chatDTO = _mapperly.ToChatDTO(ClassFake.ChatClass);
 
-var chatsDTO = ClassFake.ChatEntityList.Select(x => _mapperly.ToChatDTO(x))
-    .ToList();
+var chatsDTO = ClassFake.ChatEntityList.Select(x => _mapperly.ToChatDTO(x));
 
+foreach (var chat in chatsDTO)
+{
+    Console.WriteLine(chat.Id);
+}
