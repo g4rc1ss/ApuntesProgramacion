e07dc4d213f77401cd08b324cd5400fabaf5b0a3
Asier <asiergarciab@outlook.com>
2024-02-24 02:14:21 +0100

    refactor: :fire: Remove Python, PHP and Swift docs

--- Lenguajes/Python/ApuntesLibrosPython/Criptografia sin secretos con Python/Code/prueba.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-# -*- coding: utf-8 -*-
-
-from Code import Vigenere
-
-criptograma = 'QB HN DNKRZ RQ ZN MSGGYI RQ QHYG GSDJFQ BB QMBIIW OOCEDSKQV VC TO ZUUAS KQSYDB VAOMR CB TWQADZS'
-clave = 'MONASTERIO'
-Vigenere.generar_tabla(Vigenere.ALFABETO)
-print(Vigenere.descifrarMensaje(clave, criptograma))
