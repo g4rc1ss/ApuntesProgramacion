4cfe035a1b119477caad8a38449aa740275af1a6
Asier <asiergarciab@outlook.com>
2024-11-25 16:46:58 +0100

    Agregar Ejemplo de CancellationToken en un API

---
+++ Lenguajes/Dotnet/ApuntesNet/BackendLibraries/Performance/CancellationToken/Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", async (CancellationToken ct) =>
+{
+    try
+    {
+        foreach (var i in Enumerable.Range(0, 1000))
+        {
+            Console.WriteLine($"iteration: {i}");
+
+            await Task.Delay(i * 1000, ct); //this simulates a call to  a service
+            Console.WriteLine("Completed;");
+        }
+    }
+    catch (TaskCanceledException taskCanceledException)
+    {
+        Console.WriteLine(taskCanceledException);
+        if (ct.IsCancellationRequested)
+        {
+            Console.WriteLine("Cancelled;");
+        }
+    }
+});
+
+
+await app.RunAsync();
