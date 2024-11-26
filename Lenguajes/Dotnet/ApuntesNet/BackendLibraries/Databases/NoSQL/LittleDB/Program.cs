ea44a960477cba109f2d2c94639da11746547498
Asier <asiergarciab@outlook.com>
2024-11-26 10:05:22 +0100

    Create LiteDB example

---
+++ Lenguajes/Dotnet/ApuntesNet/BackendLibraries/Databases/NoSQL/LittleDB/Program.cs
@@ -0,0 +1,28 @@
+﻿using LiteDB;
+
+using LittleDB;
+
+
+using var liteDb = new LiteDatabase("Filename=./database.db;Password=1234");
+
+var userCollection = liteDb.GetCollection<User>("Users");
+
+var user = new User { Username = "prueba", PhoneNumber = "748347384738", };
+userCollection.Insert(user);
+
+var resultFindOne = userCollection.FindOne(x => x.Username == "prueba");
+Console.WriteLine(resultFindOne.Username);
+
+
+user.Username = "Prueba de modificacion";
+userCollection.Update(user);
+
+// Establecemos este campo como un indice para hacer la busqueda de forma mas optima
+userCollection.EnsureIndex(x => x.Username);
+
+var resultQuery = userCollection.Query()
+    .Where(x => x.Username == "Prueba de modificacion")
+    .FirstOrDefault();
+
+
+Console.WriteLine(resultQuery.Username);
---
+++ Lenguajes/Dotnet/ApuntesNet/BackendLibraries/Databases/NoSQL/LittleDB/User.cs
@@ -0,0 +1,8 @@
+namespace LittleDB;
+
+public class User
+{
+    public long Id { get; set; }
+    public string? Username { get; set; }
+    public string? PhoneNumber { get; set; }
+}
